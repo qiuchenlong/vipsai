4bb5458a2b8543d00df1a9ec45a8b33884bbb5dc
Cyndi <601976246@qq.com>
2018-03-31 17:19:42 +0800

    @更新 页面样式

--- app/src/main/java/com/vs/vipsai/main/my/MeSubAdapter.java
+++ app/src/main/java/com/vs/vipsai/main/my/MeSubAdapter.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
 import com.vs.vipsai.R;
 import com.vs.vipsai.base.adapter.BaseRecyclerAdapter;
 import com.vs.vipsai.bean.SubBean;
+import com.vs.vipsai.media.Util;
 import com.vs.vipsai.util.ImageUtils;
+import com.vs.vipsai.util.TextUtil;
 
 /**
  * Author: cynid
@@ -39,17 +41,17 @@ public class MeSubAdapter extends BaseRecyclerAdapter<SubBean> implements BaseRe
         vh.tv_time.setText("2018.02.01");
 
         if (position == 0) {
-            vh.tv_title.setText("3月28日，@张继科@景甜 正式公布恋情，张继科的这些小心思你看懂了吗？2010年3月28日他首次获得个人亚洲杯男单冠军，时隔8年在这个特殊日子高调认爱，时间刚好是景甜生日（7:21=7月21日），而328更是“相爱吧”的谐音！从微博暧昧互动到车内拥吻被拍，再到公布恋情大方撒糖，真是“科甜，可甜”！ \u200B");
-//            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_1));
-            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_1, 100, 100));
+            vh.tv_title.setText(TextUtil.tf_keyword("3月28日，@张继科@景甜 正式公布恋情，张继科的这些小心思你看懂了吗？2010年3月28日他首次获得个人亚洲杯男单冠军，时隔8年在这个特殊日子高调认爱，时间刚好是景甜生日（7:21=7月21日），而328更是“相爱吧”的谐音！从微博暧昧互动到车内拥吻被拍，再到公布恋情大方撒糖，真是“科甜，可甜”！ \u200B"));
+            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_1));
+//            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_1, 100, 100));
         } else if (position == 1) {
-            vh.tv_title.setText(Html.fromHtml("【<font color='#48AAFB'>#张继科景甜公布恋情#</font> 所以景甜会来厦门探班张继科集训吗[嘻嘻]】刚刚， 张继科微博晒出与景甜合影，正式公开恋情！恭喜恭喜！"));
-//            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_2));
-            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_2, 100, 100));
+            vh.tv_title.setText(TextUtil.tf_keyword("#张继科景甜公布恋情# 所以景甜会来厦门探班张继科集训吗[嘻嘻]】刚刚， 张继科微博晒出与景甜合影，正式公开恋情！恭喜恭喜！"));
+            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_2));
+//            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_2, 100, 100));
         } else {
-            vh.tv_title.setText("今天，回家是唯一的主题。[心]    #如你所愿#");
-//            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_3));
-            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_3, 100, 100));
+            vh.tv_title.setText(TextUtil.tf_keyword("今天，回家是唯一的主题。[心]    #如你所愿#"));
+            vh.iv_me.setImageBitmap(ImageUtils.readBitmap(mContext, R.mipmap.image_no_3));
+//            vh.iv_me.setImageBitmap(ImageUtils.decodeSampledBitmapFromResource(mContext.getResources(), R.mipmap.image_no_3, 100, 100));
         }
 
 
